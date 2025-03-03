96038722a5b23a2dead29bf72887fe92da4eca5c
Orangesplayz <ayushaarav2009@gmail.com>
2025-03-03 10:22:13 +0530

    Update tokens.js

--- tokens.js
+++ tokens.js
@@ -1,10 +1,10 @@
 const tokens = [
   {
-    token: "OTMxMTA5NzQwODU4MjA0MTgw.GrLl8i.Zy2Nl5eb_nw1CxPzMmtekVRiPTQ64-MBfEOVis",
+    token: "",
     channelIds: ["961276806676242503"], // MULTIPLE CHANNEL IDS CAN BE ADDED (OPTIONAL)
   },
   {
-    token: "OTM4MDc0NDIzODQ2NjU4MTE4.G29ooi.n1nKdPxQ-PFmMU7EP9RMklc84ioK5xJhWogwZs",
+    token: "",
     channelIds: ["961276806676242503"], // MULTIPLE CHANNEL IDS CAN BE ADDED (OPTIONAL)
   },
 ];
