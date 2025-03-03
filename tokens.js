34dd260c9ecdd0443a462eb3b043c307affc8f8d
Orangesplayz <ayushaarav2009@gmail.com>
2025-03-03 10:19:12 +0530

    Update tokens.js

--- tokens.js
+++ tokens.js
@@ -1,11 +1,11 @@
 const tokens = [
   {
-    token: "TOKEN1",
-    channelIds: ["CHANNEL1"], // MULTIPLE CHANNEL IDS CAN BE ADDED (OPTIONAL)
+    token: "OTMxMTA5NzQwODU4MjA0MTgw.GrLl8i.Zy2Nl5eb_nw1CxPzMmtekVRiPTQ64-MBfEOVis",
+    channelIds: ["961276806676242503"], // MULTIPLE CHANNEL IDS CAN BE ADDED (OPTIONAL)
   },
   {
-    token: "TOKEN2",
-    channelIds: ["CHANNEL1"], // MULTIPLE CHANNEL IDS CAN BE ADDED (OPTIONAL)
+    token: "OTM4MDc0NDIzODQ2NjU4MTE4.G29ooi.n1nKdPxQ-PFmMU7EP9RMklc84ioK5xJhWogwZs",
+    channelIds: ["961276806676242503"], // MULTIPLE CHANNEL IDS CAN BE ADDED (OPTIONAL)
   },
 ];
 
